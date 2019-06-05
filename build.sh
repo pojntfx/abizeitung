fea17a0f9e6d02041c99bc3a7a1200a1add26f7a
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-06-05 23:20:26 +0200

    refactor: Switch from water.css to Bootstrap in output site
    Former-commit-id: 852dcb0a256fbc7d5de7b72b12c58774cee96b6a

--- build.sh
+++ build.sh
@@ -23,29 +23,56 @@ cat >public/index.html <<EOF
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RvWG Abizeitung CI/CD PDFs</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>RvWG Abizeitung CI/CD PDFs</h1>
-    <p>
-        <strong style="color: red">Achtung!</strong> Dies sind <em>automatisiert kompilierte, ungeprüfte</em> "Nightly"-Versionen aus dem CI/CD-System.
-    </p>
-    <p>
-        Momentaner Status: <a href="https://gitlab.com/pojntfx/abizeitung/commits/master"><img alt="pipeline status" src="https://gitlab.com/pojntfx/abizeitung/badges/master/pipeline.svg" /></a>
-    </p>
+    <style>
+body {
+  background-image: url(https://source.unsplash.com/random?tropical) !important;
+  background-position: center center !important;
+  background-attachment: fixed !important;
+  background-repeat: no-repeat !important;
+  background-size: cover !important;
+  backdrop-filter: blur(5px) !important;
+}
+
+body > * {
+  background: linear-gradient(0deg, transparent 33%, rgba(255, 255, 255, 0.9));
+}
+    </style>
+    <div>
+    <div class="container pt-3 pb-3">
+    <h1 class="mb-3">RvWG Abizeitung CI/CD PDFs</h1>
     <p>
         Unter <a href="https://gitlab.com/pojntfx/abizeitung" target="_blank">gitlab.com/pojntfx/abizeitung</a> findet sich der Quellcode. Dies ist freie Software und freie Kultur, jeder darf gerne mitarbeiten!
     </p>
-    <ul style="overflow-x: auto;">
+    <div class="alert alert-danger">
+        <strong>Achtung!</strong> Dies sind <em>automatisiert kompilierte, ungeprüfte</em> "Nightly"-Versionen aus dem CI/CD-System.
+    </div>
+EOF
+
+cat >>public/index.html <<EOF
+    <div class='card mt-3'><div class='card-body'><h5 class='card-title'>Momentaner Status</h5>
+        <p class='card-text'><a href="https://gitlab.com/pojntfx/abizeitung/commits/master"><img alt="pipeline status" src="https://gitlab.com/pojntfx/abizeitung/badges/master/pipeline.svg" /></a><pre><code>$(git log -1)</code></pre></p>
+    </div></div>
+    <div class='card mt-3'><div class='card-body'><h5 class='card-title'>Kompilierte PDFs</h5><div class="list-group">    
 EOF
 
 for file in public/*.pdf; do
-    echo "<li><a href='${file#"public/"}'>${file#"public/"}</a></li>" >>public/index.html
+    echo "<a href='${file#"public/"}' class='list-group-item list-group-item-action'>${file#"public/"}</a>" >>public/index.html
 done
 
 cat >>public/index.html <<EOF
-    </ul>
+    </div>
+    </div></div>
+EOF
+
+echo "<div class='card mt-3'><div class='card-body'><h5 class='card-title'>Letzte Änderung</h5><p class='card-text'><pre class='pre-scrollable'><code>$(git diff --exit-code | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')</code></pre></p></div></div>" >>public/index.html
+
+cat >>public/index.html <<EOF
+    </div>
+    </div>
 </body>
 
 </html>
