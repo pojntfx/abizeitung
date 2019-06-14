f05ddc80d4f696c3a34c92fb73569d85f3ffd27e
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-06-14 11:27:43 +0200

    refactor: Improve setup instructions
    Former-commit-id: 2a61c59f3eecbbe2d271d172f85e44bd2e67990d
    
    Former-commit-id: 8238b11d7a4815fd340b1bf3d444e54eb06da8ca

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 # Apps
-flatpak remote-add --if-not-exists --no-gpg-verify chrome http://blog.kukuh.syafaat.id/google-chrome-flatpak-repo-testing/repo
-flatpak remote-add --if-not-exists firefox https://firefox-flatpak.mojefedora.cz/org.mozilla.FirefoxRepo.flatpakrepo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install chrome com.google.Chrome
-flatpak install firefox org.mozilla.FirefoxNightly
 flatpak install flathub org.gimp.GIMP
 flatpak install flathub org.libreoffice.LibreOffice
 flatpak install flathub net.scribus.Scribus
-flatpak install flathub com.visualstudio.code.oss
-flatpak install flathub io.github.liberodark.OpenDrive
+flatpak install flathub org.vim.Vim
+flatpak install flathub org.gnome.gitg
+flatpak install flathub org.gnome.Evince
 
 # Fonts
 mkdir -p ~/.local/share/fonts/
